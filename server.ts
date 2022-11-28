83119607ef514ae885f5adc9389314e3b939bd37
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-11-28 16:38:26 -0500

    New Mongo connection string

--- server.ts
+++ server.ts
@@ -15,19 +15,22 @@ import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
 import mongoose from "mongoose";
+import { convertCompilerOptionsFromJson } from 'typescript';
 var cors = require('cors')
 
 // build the connection string
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-//THIS NEEDS TO CHANGE!
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+const HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}`;
+
 // connect to the database
-mongoose.connect(connectionString);
+
+//TO-DO 
+//mongoose.connect(connectionString);
+mongoose.connect('mongodb+srv://nshah:nshah@cluster0.egczgje.mongodb.net/tuiter-project')
 
 const app = express();
 app.use(express.json());
