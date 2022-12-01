8d4c944c194ac430c5b403243b0dab4b1211fad6
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-11-30 22:49:57 -0500

    Adding admin interface

---
+++ interfaces/AdminControllerI.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from "express";
+
+export default interface AdminControllerI {
+    findAllUsers(req: Request, res: Response): void;
+    //createUser(req: Request, res: Response): void;
+    //updateUser(req: Request, res: Response): void;
+    //deleteUser(req: Request, res: Response): void;
+    //blockUser(req: Request, res: Response): void;
+};
