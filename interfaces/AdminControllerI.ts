7c55098f9c527bc49b65f5b71d40eeaa1646783e
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-12-02 20:39:55 -0500

    Adding block user api

--- interfaces/AdminControllerI.ts
+++ interfaces/AdminControllerI.ts
@@ -2,4 +2,5 @@ import {Request, Response} from "express";
 
 export default interface AdminControllerI {
     findAllUsers(req: Request, res: Response): void;
+    blockUser(req: Request, res: Response): void;
 };
